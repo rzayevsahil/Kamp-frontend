ee2919a306ebd60d298fb2005a846b148bb51ee0
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-03-15 00:29:48 +0400

    updated

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -9,7 +9,7 @@ export class ProductComponent implements OnInit {
 
   product1 = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 };
   product2 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 };
-  product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 5 };
+  product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 6 };
   product4 = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 };
   product5 = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5 };
 
