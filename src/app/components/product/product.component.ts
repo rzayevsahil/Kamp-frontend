6e8b5cd11e9ebdf807158050ab2b35564c80f2c0
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-03-15 19:22:14 +0400

    updated

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -7,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product1 = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 };
-  product2 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 };
-  product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 6 };
-  product4 = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 };
-  product5 = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5 };
+  // product1 = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5, unitsInStock: 5 };
+  // product2 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5, unitsInStock: 5 };
+  // product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 5, unitsInStock: 5 };
+  // product4 = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5, unitsInStock: 5 };
+  // product5 = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5, unitsInStock: 5 };
 
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
+  products:Product[] = [];
   constructor() { }
 
   ngOnInit(): void {
