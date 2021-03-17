256b02cb779085d15448ffbc33e0f106d4b80b8c
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-03-17 21:36:23 +0400

    update

--- src/app/models/categoryResponseModel.ts
+++ src/app/models/categoryResponseModel.ts
@@ -1,5 +1,5 @@
 import { Category } from "./category";
-import { ResponseModel } from "./ResponseModel";
+import { ResponseModel } from "./responseModel";
 
 export interface CategoryResponseModel extends ResponseModel{
     data:Category[]  
